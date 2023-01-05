808bd0fe0c7b53f23ed993cfb316b5a2fa1d849a
Phani Adi <122045626+APK444@users.noreply.github.com>
2023-01-05 12:54:47 -0600

    Update streamlit_app.py
    added from training

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Healthy Diner')
-streamlit.header('Menu')
-streamlit.text('Oat Meal')
-streamlit.text('Smoothie')
-streamlit.text('Boiled Eggs')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
