ef673f1c40a42e3630ce99291462676b42b05030
Phani Adi <122045626+APK444@users.noreply.github.com>
2023-01-06 13:13:54 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,13 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Healthy Diner')
 streamlit.header('Breakfast Favs')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avacado Toast')
+streamlit.text('🥑🍞 Avocado Toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
@@ -19,4 +20,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
  
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 
